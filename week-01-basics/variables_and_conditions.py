938e5e5325190f10e92928cf1eb49d3be2fc2ef9
Illia <lisoilka27@gmail.com>
2025-06-29 00:05:10 +0200

    Create variables_and_conditions.py

---
+++ week-01-basics/variables_and_conditions.py
@@ -0,0 +1,17 @@
+#It's my first week and I started writing it on only third day of my learning!
+z = 1 #variables basis: "Name = Data"
+print(z) #print syntax
+#many types of variables
+x = int(1) #integer
+print(type(x))
+c = str(1) #string
+print(type(c))
+v = float(1) #number with .00
+print(type(v))
+b = complex(1j)
+print(type(b))
+#massive like
+n = ["cherry", "burger", "ferger", 3, True] #list of any data
+print(type(n))
+print(n)
+print(len(n)) #len syntax for measuring legth
